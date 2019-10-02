ff2753b9c5345312157cc921f025d647c8bbbffa
Nguyen Tung <nguyeentung@gmail.com>
2019-10-02 15:59:15 +0700

    Init application

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class Api::V1::User::SessionsController < Devise::SessionsController
+  before_action :configure_sign_in_params, only: [:create]
+
+  # POST /user/sign_in
+  def create
+    super
+  end
+
+  # DELETE /user/sign_out
+  def destroy
+    super
+  end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  end
+end
